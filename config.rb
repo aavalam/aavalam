06619de95f100077db5b9a1d90ece8b0d3538088
Victor Goff <keeperotphones@gmail.com>
2013-02-16 05:30:40 -0600

    Added configuration changes

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/aavalam/"
+http_images_path = "/aavalam/images"
+http_fonts_path = "/aavalam/fonts"
+css_dir = "public/aavalam/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
---
+++ source/_posts/2013-02-16-saying-goodbye-to-posterous.markdown
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "Saying Goodbye to Posterous"
+date: 2013-02-16 05:26
+comments: true
+categories: [Blog, Posterous, Octopress]
+---
+
